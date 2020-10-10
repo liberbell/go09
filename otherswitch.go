69dc1d5669225e6d692e315fdd5a77167ff58dd8
Liberbell <liberbell@gmail.com>
2020-10-10 11:21:53 +0900

    otherswitch3

--- otherswitch.go
+++ otherswitch.go
@@ -7,7 +7,7 @@ import (
 
 func main() {
 	switch time.Now().Weekday() {
-	case time.Monday, time.Thuesday, time.Wednesday, time.Thursday, time.Friday:
+	case time.Monday, time.Tuesday, time.Wednesday, time.Thursday, time.Friday:
 		fmt.Println("It's the weekday.")
 	default:
 		fmt.Println("It's the weekend.")
