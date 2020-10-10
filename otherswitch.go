c7ab6457a0994babe475ee081025d74d6c8b5a90
Liberbell <liberbell@gmail.com>
2020-10-10 11:20:30 +0900

    otherswitch2

---
+++ otherswitch.go
@@ -0,0 +1,14 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	switch time.Now().Weekday() {
+	case time.Monday, time.Thuesday, time.Wednesday, time.Thursday, time.Friday:
+		fmt.Println("It's the weekday.")
+	default:
+		fmt.Println("It's the weekend.")
+	}
+}
